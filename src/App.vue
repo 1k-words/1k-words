c11216be43384fdb18779915357fec92fef397ea
Mark Parson <mark@nanobox.io>
2018-03-17 11:42:58 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <a href="mailto:hello@1k-pictures.com">
+      <img src="./assets/compiled/logo.svg">
+    </a>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body{background: url("./assets/background-temp.jpg") no-repeat center center fixed; background-size: cover; }
+img{width:135px; position: absolute; margin: auto auto; left:0; right:0; top:0; bottom:0; }
+</style>
